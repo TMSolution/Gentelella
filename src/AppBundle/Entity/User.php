f6ec4e33f5f46dcef44fb8155852eca9d7507c78
Krzysiek Piasecki <krzysiekpiasecki@gmail.com>
2016-04-17 23:37:35 +0200

    Install and basic configuration of FOSUserBundle

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as FOSUser;
+
+/**
+ * User
+ *
+ * @ORM\Table(name="app_user")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\UserRepository")
+ */
+class User extends FOSUser
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * User constructor
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
